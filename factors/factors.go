9b4f152be11865f2c670bf2077ff8b0c0ad1f1a4
dvberkel <daan.v.berkel.1980@gmail.com>
2012-11-22 16:23:42 +0100

    create factor package

---
+++ factors/factors.go
@@ -0,0 +1,7 @@
+package factors
+
+func Of(n int) []int {
+     result := make([]int, 1)
+     result[0] = 2
+     return result
+}
--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/dvberkel/go-factorization/factors"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Printf("Factors of 2 : %v\n", factors.Of(2))
 }
